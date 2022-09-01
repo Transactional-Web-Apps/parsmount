f6fc35024b70b182c99cdb06860395522959f16d
Mahsa Roostaei <2195420@edu.vaniercollege.qc.ca>
2022-09-01 11:49:38 -0400

    AboutUs page

---
+++ views/aboutus.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login DataBase mySQL PHP</title>
+
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="icon.jpg" type="image/x-jpg">
+	<link rel="stylesheet" type= "text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" >
+	<link rel="stylesheet" href="parsmount.css">
+	<title>Parsmount Website - Sign in</title>
+</head>
+<body>
+
+<header>
+        Parsmount
+    </header>
+
+    <nav>
+ 
+        <a href="index.php">Home</a>&nbsp;&nbsp;
+        <a href="event.php"id="current">About us</a>&nbsp;&nbsp;       
+        <a href="contact.php">Contact</a>&nbsp;&nbsp;
+		<a href="signin.php">Sign in</a>&nbsp;&nbsp;
+		<a href="signup.php">Sign up</a>&nbsp;&nbsp;
+    </nav>
+
+<div class="container">
+
+<section>
+        
+        <div>
+       <h3 style="text-align: center;">About Us</h3>
+      
+           <video src="Resources/aboutvideo.MP4" width="620" height="340" controls="controls">
+    
+   </div>
+
+
+   <div>
+      
+       <strong style="padding: 45px">What is Parsmount?</strong><br />
+       <p style="padding: 45px">
+           Lorem Ipsum is simply dummy text of the printing and
+   typesetting industry. Lorem Ipsum has been the
+   industry; standard dummy text ever since the
+   1500s, when an unknown printer took a galley of type and
+   scrambled it to make a type specimen book. It has
+   survived not only five centuries, but also the leap into
+   electronic typesetting, remaining essentially unchanged.
+   It was popularised in the 1960s with the release of
+   Letraset sheets containing Lorem Ipsum passages, and
+   more recently with desktop publishing software like
+   Aldus PageMaker including versions of Lorem Ipsum.
+       </p>
+      
+   </div>
+  </section>
+<br><br>
+<footer>
+        <p><i>
+                Copyright &copy; 2022 Parsmount <br>
+                <a href="mailto:yourfirstname@yourlastname.com">yourfirstname@yourlastname.com</a>
+            </i></p>
+    </footer>
+</body>
+</html>
