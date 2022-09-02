021adcabf6fc0f1f83d8b67563eff5c3866cece6
Joseph Jananji <josephuniv@hotmail.com>
2022-09-01 22:58:16 -0400

    add config file

--- views/index.php
+++ views/index.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="icon.jpg" type="image/x-jpg">
-    <link rel="stylesheet" href="parsmount.css">
+    <link rel="stylesheet" href="../parsmount.css">
     <title>Parsmount Website - Home</title>
 </head>
 
