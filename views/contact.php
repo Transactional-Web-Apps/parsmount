e14b53811a4ac1dd7dda45ad7f9c6cf1c14c1ce8
Joseph Jananji <josephuniv@hotmail.com>
2022-09-01 22:53:04 -0400

    Add folders

--- views/contact.php
+++ views/contact.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="icon.jpg" type="image/x-jpg">
 	<link rel="stylesheet" type= "text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" >
-	<link rel="stylesheet" href="parsmount.css">
+	<link rel="stylesheet" href="../parsmount.css">
 	<title>Parsmount Website - Contact</title>
     <style>
         div {
