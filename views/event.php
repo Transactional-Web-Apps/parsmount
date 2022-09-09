dae988dbd2b07d78e201333bcbdd6bf35a71b69a
Mahsa Roostaei <2195420@edu.vaniercollege.qc.ca>
2022-09-09 12:36:18 -0400

    index page and event page

--- views/event.php
+++ views/event.php
@@ -10,6 +10,7 @@
     <link rel="icon" href="icon.jpg" type="image/x-jpg">
     <link rel="stylesheet" type= "text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" >
     <link rel="stylesheet" href="../Resources/parsmount.css">
+    <link rel="stylesheet" href="style.css" />
     <title>Parsmount Website - Event</title>
 </head>
 
@@ -27,7 +28,175 @@
     </nav>
 
     <main>
-       
+    <img alt=""
+            class="logo-png-1"
+            src="../Resources/logo.png" />
+    <h3>Events</h3>
+
+
+<div class="component-4">
+    <div class="relative-wrapper-one">
+        <p class="lorem-ipsum-is-simply-dummy-text-of-the">
+            Camping Oka
+            Sat, Aug 13, 12:00 PM    
+            <br/>
+            - Mon, Aug 15, 2022,
+            <br/>
+            21:00 PM. Oka National Park • Oka, QC
+            <br/>
+            Price: $25,50
+            <br/>
+            Family Plan (Children allowed)
+        </p>
+        <div class="flex-wrapper-three">
+            <img alt=""
+                class="sports-pic"
+                src="https://static.overlay-tech.com/assets/93dcc6c4-c1f1-4f8e-bca0-d781a7dc6171.png" />
+        </div>
+    </div>
+    <div class="relative-wrapper-four">
+        <p class="lorem-ipsum-is-simply-dummy-text-of-the">
+            Camping Oka
+            Sat, Aug 13, 12:00 PM    
+            <br/>
+            - Mon, Aug 15, 2022,
+            <br/>
+            21:00 PM. Oka National Park • Oka, QC
+            <br/>
+            Price: $25,50
+            <br/>
+            Family Plan (Children allowed)
+        </p>
+        <div class="flex-wrapper-four">
+
+            <img alt=""
+                class="group-of-friends-trekking-with-backpacks"
+                src="https://static.overlay-tech.com/assets/b6024f9d-ccd2-4411-9fd3-cbe40b892d2b.png" />
+        </div>
+    </div>
+    <div class="relative-wrapper-two">
+        <p class="lorem-ipsum-is-simply-dummy-text-of-the-two">
+         Camping Oka
+            Sat, Aug 13, 12:00 PM    
+            <br/>
+            - Mon, Aug 15, 2022,
+            <br/>
+            21:00 PM. Oka National Park • Oka, QC
+            <br/>
+            Price: $25,50
+            <br/>
+            Family Plan (Children allowed)
+        </p>
+        <div class="flex-wrapper-one">
+            <img alt=""
+                class="ready-for-the-concert-2021-08-27-15-02-3"
+                src="https://static.overlay-tech.com/assets/732b9429-b9de-4597-9bcf-cd30c9f65e38.png" />
+        </div>
+    </div>
+    <div class="relative-wrapper-one">
+        <p class="lorem-ipsum-is-simply-dummy-text-of-the">
+     Camping Oka
+            Sat, Aug 13, 12:00 PM    
+            <br/>
+            - Mon, Aug 15, 2022,
+            <br/>
+            21:00 PM. Oka National Park • Oka, QC
+            <br/>
+            Price: $25,50
+            <br/>
+            Family Plan (Children allowed)
+        </p>
+        <div class="flex-wrapper-two">
+
+            <img alt=""
+                class="well-dressed-people-celebrating-new-year"
+                src="https://static.overlay-tech.com/assets/bf022624-52a6-4d30-a86e-e48245c92c73.png" />
+        </div>
+    </div>
+</div>
+
+
+<div class="component-4">
+    <div class="relative-wrapper-one">
+        <p class="lorem-ipsum-is-simply-dummy-text-of-the">
+    Camping Oka
+            Sat, Aug 13, 12:00 PM    
+            <br/>
+            - Mon, Aug 15, 2022,
+            <br/>
+            21:00 PM. Oka National Park • Oka, QC
+            <br/>
+            Price: $25,50
+            <br/>
+            Family Plan (Children allowed)
+        </p>
+        <div class="flex-wrapper-three">
+            <img alt=""
+                class="sports-pic"
+                src="https://static.overlay-tech.com/assets/93dcc6c4-c1f1-4f8e-bca0-d781a7dc6171.png" />
+        </div>
+    </div>
+    <div class="relative-wrapper-four">
+        <p class="lorem-ipsum-is-simply-dummy-text-of-the">
+      Camping Oka
+            Sat, Aug 13, 12:00 PM    
+            <br/>
+            - Mon, Aug 15, 2022,
+            <br/>
+            21:00 PM. Oka National Park • Oka, QC
+            <br/>
+            Price: $25,50
+            <br/>
+            Family Plan (Children allowed)
+        </p>
+        <div class="flex-wrapper-four">
+
+            <img alt=""
+                class="group-of-friends-trekking-with-backpacks"
+                src="https://static.overlay-tech.com/assets/b6024f9d-ccd2-4411-9fd3-cbe40b892d2b.png" />
+        </div>
+    </div>
+    <div class="relative-wrapper-two">
+        <p class="lorem-ipsum-is-simply-dummy-text-of-the-two">
+       Camping Oka
+            Sat, Aug 13, 12:00 PM    
+            <br/>
+            - Mon, Aug 15, 2022,
+            <br/>
+            21:00 PM. Oka National Park • Oka, QC
+            <br/>
+            Price: $25,50
+            <br/>
+            Family Plan (Children allowed)
+        </p>
+        <div class="flex-wrapper-one">
+
+            <img alt=""
+                class="ready-for-the-concert-2021-08-27-15-02-3"
+                src="https://static.overlay-tech.com/assets/732b9429-b9de-4597-9bcf-cd30c9f65e38.png" />
+        </div>
+    </div>
+    <div class="relative-wrapper-one">
+        <p class="lorem-ipsum-is-simply-dummy-text-of-the">
+    Camping Oka
+            Sat, Aug 13, 12:00 PM    
+            <br/>
+            - Mon, Aug 15, 2022,
+            <br/>
+            21:00 PM. Oka National Park • Oka, QC
+            <br/>
+            Price: $25,50
+            <br/>
+            Family Plan (Children allowed)
+        </p>
+        <div class="flex-wrapper-two">
+
+            <img alt=""
+                class="well-dressed-people-celebrating-new-year"
+                src="https://static.overlay-tech.com/assets/bf022624-52a6-4d30-a86e-e48245c92c73.png" />
+        </div>
+    </div>
+</div> 
     </main>
 
     <footer>
--- views/index.php
+++ views/index.php
@@ -10,6 +10,7 @@
     <link rel="icon" href="icon.jpg" type="image/x-jpg">
     <link rel="stylesheet" type= "text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" >
     <link rel="stylesheet" href="../Resources/parsmount.css">
+    <link rel="stylesheet" href="style.css" />
     <title>Parsmount Website - Home</title>
 </head>
 
@@ -27,7 +28,99 @@
     </nav>
 
     <main>
-       
+   
+    <img alt=""
+            class="logo-png-1"
+            src="../Resources/logo.png" />
+           
+    <div class="wrap">
+        <div class="bannertext>">
+            <h1>Adventure Without Limit</h1>
+            <h1>With Parsmount</h1>
+        </div>
+        
+    </div>
+    
+    
+    <div class="middlebanner">
+    <img src="../Resources/MiddleBanner.jpeg"  Width="300" Height="300" />
+        <div class="bannertext>">
+            <p class="bt">
+                By holding one hundred and fifty sports programs
+                and nature trips with the presence of more than eight
+                hundred active members, Parsmont is a symbol of unity,
+                empathy and solidarity of the Canadian Farsi-speaking
+                community.
+            </p>
+        </div>
+    </div>
+
+    <h3>Upcoming Events</h3>
+
+    <div class="component-4">
+        <div class="relative-wrapper-one">
+            <p class="lorem-ipsum-is-simply-dummy-text-of-the">
+                Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the
+                industry&#039;s standard dummy text ever since the
+                1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book.
+            </p>
+            <div class="flex-wrapper-three">
+
+
+                <img alt=""
+                    class="sports-pic"
+                    src="https://static.overlay-tech.com/assets/93dcc6c4-c1f1-4f8e-bca0-d781a7dc6171.png" />
+            </div>
+        </div>
+        <div class="relative-wrapper-four">
+            <p class="lorem-ipsum-is-simply-dummy-text-of-the">
+                Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the
+                industry&#039;s standard dummy text ever since the
+                1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book.
+            </p>
+            <div class="flex-wrapper-four">
+
+                <img alt=""
+                    class="group-of-friends-trekking-with-backpacks"
+                    src="https://static.overlay-tech.com/assets/b6024f9d-ccd2-4411-9fd3-cbe40b892d2b.png" />
+            </div>
+        </div>
+        <div class="relative-wrapper-two">
+            <p class="lorem-ipsum-is-simply-dummy-text-of-the-two">
+                Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the
+                industry&#039;s standard dummy text ever since the
+                1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book.
+            </p>
+            <div class="flex-wrapper-one">
+
+                <img alt=""
+                    class="ready-for-the-concert-2021-08-27-15-02-3"
+                    src="https://static.overlay-tech.com/assets/732b9429-b9de-4597-9bcf-cd30c9f65e38.png" />
+            </div>
+        </div>
+        <div class="relative-wrapper-one">
+            <p class="lorem-ipsum-is-simply-dummy-text-of-the">
+                Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the
+                industry&#039;s standard dummy text ever since the
+                1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book.
+            </p>
+            <div class="flex-wrapper-two">
+
+                <img alt=""
+                    class="well-dressed-people-celebrating-new-year"
+                    src="https://static.overlay-tech.com/assets/bf022624-52a6-4d30-a86e-e48245c92c73.png" />
+            </div>
+        </div>
+    </div>
+
 
 
     </main>
@@ -38,6 +131,34 @@
                 <a href="mailto:yourfirstname@yourlastname.com">yourfirstname@yourlastname.com</a>
             </i></p>
     </footer>
+    <script>
+        let slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+            if (n > slides.length) { slideIndex = 1 }
+            if (n < 1) { slideIndex = slides.length }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
+    </script>
 
 </body>
 
